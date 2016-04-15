b8281ea7d5bb6fcfd5c09c6480ff6b4167321b82
lukegjpotter <lukegjpotter@gmail.com>
2016-04-15 23:24:28 +0100

    Got ParsingLoop Tests working.

--- src/main/java/com/lukegjpotter/spring/application/parse/ParsingLoop.java
+++ src/main/java/com/lukegjpotter/spring/application/parse/ParsingLoop.java
@@ -1,12 +1,18 @@
 package com.lukegjpotter.spring.application.parse;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.lukegjpotter.spring.application.model.RoadRaceEvent;
+import com.lukegjpotter.spring.application.util.Constants;
 
 @Component
 public class ParsingLoop {
@@ -15,47 +21,42 @@ public class ParsingLoop {
     @Autowired private DescriptionParser descriptionParser;
     @Autowired private StageDetailParser stageDetailParser;
 
-    public List<RoadRaceEvent> startParseLoop(String fileLocation){
-        
+    public List<RoadRaceEvent> startParseLoop(String fileLocation) {
+
         List<RoadRaceEvent> roadRaces = new ArrayList<>();
-        
-        int size = 1, i = 0;
-        if (fileLocation.equals("./src/test/resources/TwoRaces.html")) size = 2;
-        
-        while (i < size) {
-            RoadRaceEvent roadRace = headerParser.parse("");
-            roadRace.addDescription(descriptionParser.parse(""));
-            roadRace.setStageDetails(stageDetailParser.parse(""));
-            roadRaces.add(roadRace);
-            i++;
+
+        Element document = null;
+        try {
+            document = Jsoup.parse(new File(fileLocation), Constants.FILE_FORMAT);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        
-        
-        /*Element document = Jsoup.parseBodyFragment(utils.readFile(fileLocation));
-        Elements allEvents = document.getElementsByTag("");
-        
+        Elements allEventsTable = document.select("table#AllEventsTable");
+        Element table = allEventsTable.first();
+        Elements allEvents = table.getElementsByTag("tr");
+
         boolean isRacePopulated = false, isRaceHeaderSet = false;
         RoadRaceEvent roadRace = new RoadRaceEvent();
-        
+
         for (Element event : allEvents) {
-            
-            if (event.attr("style").contains("blackground-color")) {
+
+            if (event.hasAttr("style")) {
                 roadRace = headerParser.parse(event.html());
                 isRaceHeaderSet = true;
-            } else if(isRaceHeaderSet) {
+            } else if (isRaceHeaderSet) {
                 roadRace.addDescription(descriptionParser.parse(event.html()));
                 roadRace.setStageDetails(stageDetailParser.parse(event.html()));
                 isRacePopulated = true;
             }
-            
+
             if (isRaceHeaderSet && isRacePopulated) {
                 roadRaces.add(roadRace);
                 roadRace = new RoadRaceEvent();
                 isRaceHeaderSet = false;
                 isRacePopulated = false;
             }
-        }*/
-        
+        }
+
         return roadRaces;
     }
 }

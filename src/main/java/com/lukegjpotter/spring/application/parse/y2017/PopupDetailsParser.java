f0a465c682da2eb0fa8cccea0a8cea24c792741b
lukegjpotter <lukegjpotter@gmail.com>
2017-02-23 13:14:31 +0000

    Parsing Popup's Start Date

--- src/main/java/com/lukegjpotter/spring/application/parse/y2017/PopupDetailsParser.java
+++ src/main/java/com/lukegjpotter/spring/application/parse/y2017/PopupDetailsParser.java
@@ -1,11 +1,14 @@
 package com.lukegjpotter.spring.application.parse.y2017;
 
+import java.util.Date;
+
 import org.jsoup.nodes.Element;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.lukegjpotter.spring.application.model.PopupDetails;
-import com.lukegjpotter.spring.application.model.RoadRaceEvent;
 import com.lukegjpotter.spring.application.parse.Parsable;
+import com.lukegjpotter.spring.application.util.UtilsService;
 
 /**
  * Extracts the Date, Time, Province, Category, Promoting Club, Contact
@@ -15,14 +18,26 @@ import com.lukegjpotter.spring.application.parse.Parsable;
  */
 @Component
 class PopupDetailsParser implements Parsable<Element, PopupDetails> {
+    
+    @Autowired UtilsService utils;
 
     @Override public PopupDetails parse(Element htmlElementToParse) {
         
         PopupDetails popupDetails = new PopupDetails();
         
+        popupDetails.setStartDate(extractPopupDate(htmlElementToParse));
         popupDetails.setProvince(htmlElementToParse.getElementsByAttributeValue("href", "#location0").first().text().trim());
         
         return popupDetails;
     }
-
+    
+    private Date extractPopupDate(Element element) {
+        
+        String rawDateString = element.getElementById("event_date").text();
+        int indexOfSeperator = rawDateString.indexOf("|");
+        int indexOfComma = rawDateString.indexOf(",") + 1;
+        String dateString = rawDateString.substring(indexOfComma, indexOfSeperator).trim();
+        
+        return utils.convertMMMMDDYYYYToDate(dateString);
+    }
 }
--- src/test/java/com/lukegjpotter/spring/application/parse/y2017/PopupDetailsParserTest.java
+++ src/test/java/com/lukegjpotter/spring/application/parse/y2017/PopupDetailsParserTest.java
@@ -4,8 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
-
-import javax.swing.Popup;
+import java.util.Date;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
@@ -18,14 +17,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.lukegjpotter.spring.application.CyclingIrelandEventsHtmlScraperApplication;
 import com.lukegjpotter.spring.application.model.PopupDetails;
-import com.lukegjpotter.spring.application.model.RoadRaceEvent;
 import com.lukegjpotter.spring.application.util.Constants;
+import com.lukegjpotter.spring.application.util.UtilsService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = { CyclingIrelandEventsHtmlScraperApplication.class, PopupDetailsParser.class })
 public class PopupDetailsParserTest {
 
     @Autowired PopupDetailsParser popupDetailsParser;
+    @Autowired UtilsService utils;
     
     @Before public void setUp() throws Exception {}
 
@@ -33,6 +33,9 @@ public class PopupDetailsParserTest {
         
         PopupDetails popup = popupDetailsParser.parse(getJsoupElementFromPopup());
         
+        Date expectedDate = utils.convertMMMMDDYYYYToDate("February 25, 2017");
+        
+        assertTrue(expectedDate.equals(popup.getStartDate()));
         assertTrue(popup.getProvince().equals("Leinster"));
     }
     

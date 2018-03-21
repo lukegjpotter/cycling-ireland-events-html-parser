1fc97d8d6c8aba4cd85a6dc7c5af583b58647239
lukegjpotter <lukegjpotter@gmail.com>
2018-03-21 18:57:38 +0000

    Issue #8 non-Parse Failing Tests: StageRouteMappingHolder.
    Also some #9

--- src/main/java/com/lukegjpotter/spring/application/model/StageRouteMappingHolder.java
+++ src/main/java/com/lukegjpotter/spring/application/model/StageRouteMappingHolder.java
@@ -1,6 +1,5 @@
 package com.lukegjpotter.spring.application.model;
 
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,12 +14,12 @@ public class StageRouteMappingHolder {
 
     public void putRouteUrlMapping(Long keyEventId, List<String> valueRoutes) {
         nameDateRouteMap.put(keyEventId, valueRoutes);
-   }
+    }
 
     public String getRouteUrlMapping(Long keyEventId, String stageNumberQuery) {
         try {
-            return nameDateRouteMap.get(keyEventId).get(Integer.getInteger(stageNumberQuery) - 1);
-        } catch (ArrayIndexOutOfBoundsException e) {
+            return nameDateRouteMap.get(keyEventId).get(Integer.parseInt(stageNumberQuery) - 1);
+        } catch (IndexOutOfBoundsException | NumberFormatException | NullPointerException ignored) {
             return "";
         }
     }
@@ -30,16 +29,4 @@ public class StageRouteMappingHolder {
         return o instanceof StageRouteMappingHolder
                 && this.nameDateRouteMap.equals(((StageRouteMappingHolder) o).nameDateRouteMap);
     }
-
-    public void putRouteUrlMapping(String eventName, Date date, List<String> routes) {
-        // TODO Delete this method after more tests are implemented for the 2017
-        //      format, it's only here to stop the compiler complaining.
-    }
-    
-    public String getRouteUrlMapping(String eventName, Date startDate, Integer stageNumber) {
-        // TODO Delete this method after more tests are implemented for the 2017
-        //      format, it's only here to stop the compiler complaining.
-        return "";
-    }
-
 }
--- src/test/java/com/lukegjpotter/spring/application/model/StageRouteMappingHolderTest.java
+++ src/test/java/com/lukegjpotter/spring/application/model/StageRouteMappingHolderTest.java
@@ -3,15 +3,17 @@ package com.lukegjpotter.spring.application.model;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 public class StageRouteMappingHolderTest {
 
     @Test public void testStageRouteMappingHolder() {
@@ -24,5 +26,52 @@ public class StageRouteMappingHolderTest {
 
         assertTrue(holder.getRouteUrlMapping(eventId, "1").equals(stage1));
         assertTrue(holder.getRouteUrlMapping(eventId, "2").equals(stage2));
+    }
+
+    @Test public void testStageRouteMappingHolderNonExistingStages() {
+        long eventId = 1L;
+        String stage1 = "stage1";
+        List<String> routes = Collections.singletonList(stage1);
+
+        StageRouteMappingHolder holder = new StageRouteMappingHolder();
+        holder.putRouteUrlMapping(eventId, routes);
+
+        assertTrue(holder.getRouteUrlMapping(eventId, "5").equals(""));
+    }
+
+    @Test public void testStageRouteMappingHolderNonExistingEventId() {
+        long eventId = 1L;
+        String stage1 = "stage1";
+        List<String> routes = Collections.singletonList(stage1);
+
+        StageRouteMappingHolder holder = new StageRouteMappingHolder();
+        holder.putRouteUrlMapping(eventId, routes);
+
+        assertTrue(holder.getRouteUrlMapping(5L, "5").equals(""));
+    }
+
+    @Test public void testStageRouteMappingHolderCrazyStageNumber() {
+        long eventId = 1L;
+        String stage1 = "stage1";
+        List<String> routes = Collections.singletonList(stage1);
+
+        StageRouteMappingHolder holder = new StageRouteMappingHolder();
+        holder.putRouteUrlMapping(eventId, routes);
+
+        assertTrue(holder.getRouteUrlMapping(eventId, "crazy").equals(""));
+    }
+
+    @Test public void testStageRouteMappingHolderEquals() {
+        long eventId = 1L;
+        String stage1 = "stage1";
+        List<String> routes = Collections.singletonList(stage1);
+
+        StageRouteMappingHolder holder1 = new StageRouteMappingHolder();
+        holder1.putRouteUrlMapping(eventId, routes);
+        StageRouteMappingHolder holder2 = new StageRouteMappingHolder();
+        holder2.putRouteUrlMapping(eventId, routes);
+
+        assertFalse(holder1.equals(null));
+        assertTrue(holder1.equals(holder2));
     }
 }
